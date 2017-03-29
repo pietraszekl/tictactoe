daa430983820f456449c3f87822461a99cecaad9
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-03-29 21:33:32 +0100

    adding headers and score logic

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <div id="app">
-    <div id="details">
-      <h1>Tic Tac Toe</h1>
+  <div>
+    <div class="scoreBoard">
+      <span>O has {{ wins.O }} wins</span>
+      <h2>Score Board</h2>
+      <span>X has {{ wins.X }} wins</span>
     </div>
-    <grid></grid>
+    <div id="app">
+      <div id="details">
+        <h1>Tic Tac Toe</h1>
+        <h2>Match #{{ matches + 1 }}</h2>
+      </div>
+      <grid></grid>
+      <button class="restart"
+              @click="restart">Restart</button>
+    </div>
   </div>
 </template>
 
 <script>
-import Grid from './components/Grid.vue';
+import Grid from './components/Grid.vue'
 
 export default {
-  name: 'app',
   components: { Grid },
+  name: 'app',
   data() {
     return {
       matches: 0,
@@ -21,36 +31,87 @@ export default {
         X: 0
       }
     }
+  },
+
+  methods: {
+    restart() {
+      Event.$emit('clearCell')
+
+      Event.$emit('gridReset')
+
+      this.matches++
+    }
+  },
+
+  created() {
+    Event.$on('win', winner => this.wins[winner]++)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+body {
+  background-color: #fff;
+  color: #fff;
+  font-family: 'Dosis', Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
 }
 
-h1,
-h2 {
-  font-weight: normal;
+#app {
+  margin: 0 auto;
+  max-width: 270px;
+  color: #34495e;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 3em;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.restart {
+  background-color: #e74c3c;
+  color: #fff;
+  border: 0px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  font-family: 'Dosis', Helvetica, sans-serif;
+  font-size: 1.4em;
+  font-weight: bold;
+  margin: 0px;
+  padding: 15px;
+  width: 100%;
 }
 
-a {
-  color: #42b983;
+.restart:hover {
+  background-color: #c0392b;
+  cursor: pointer;
+}
+
+.scoreBoard {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 15px;
+  background-color: #16a085;
+  box-shadow: 10px solid #fff;
+  padding: 20px;
+  overflow-x: none;
+}
+
+.scoreBoard h2 {
+  margin: 0px;
+}
+
+.scoreBoard span {
+  float: right;
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-left: 20px;
 }
 </style>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,12 +1,44 @@
 <template>
-  <td class="cell">
-    {{ mark }}
-  </td>
+  <td class="cell"
+      @click="strike">
+    {{ mark }}</td>
 </template>
 
 <script>
 export default {
-  data() { }
+  props: ['name'],
+  data() {
+    return {
+      // enables the player to place a mark
+      frozen: false,
+      // holds either X or O to be displayed in the td
+      mark: ''
+    }
+  },
+
+  methods: {
+    strike() {
+      if (!this.frozen) {
+        // gets either X or O from the Grid component
+        this.mark = this.$parent.activePlayer
+
+        this.frozen = true
+
+        // fires an event to notify the Grid component that a mark is placed
+        Event.$emit('strike', this.name)
+      }
+    }
+  },
+
+  created() {
+    Event.$on('clearCell', () => {
+      this.mark = ''
+
+      this.frozen = false
+    })
+
+    Event.$on('freeze', () => this.frozen = true)
+  }
 }
 </script>
 
